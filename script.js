f8f1141b5095ab5bea8377a49fa48dae7ad8c94e
Pankaj Bisht <work.pankajbisht@gmail.com>
2025-09-10 21:49:22 +0530

    added js with html

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
 const key=1;
 
-const url="https://www.omdbapi.com/?t=avengers&plot=full&apikey=7f47f75b";
 
-async function getData() {
+
+const title =document.querySelector("#title");
+const rating =document.querySelector("#rating");
+const genre=document.querySelector("#genre");
+const actors=document.querySelector("#actors");
+const plot=document.querySelector("#plot");
+const poster = document.querySelector("#poster");
+const search_Btn =document.querySelector("#search_Btn");
+const search_Input =document.querySelector("#search_Input");
+
+
+
+search_Btn.addEventListener("click",()=>{
+    let movie_name= search_Input.value;
+    const url=`https://www.omdbapi.com/?t=${movie_name}&plot=full&apikey=7f47f75b`;
+    getData(url)
+})
+async function getData(url) {
     try{
     let req= await fetch(url);
     let data=await req.json();
-    console.log(data);        
+
+    title.innerText=data.Title +`(${data.Year})`;
+    rating.innerHTML="Rating : "+data.imdbRating;
+       
+    genre.innerText=data.Genre;
+    
+    actors.innerText=data.Actors; 
+    
+    plot.innerText=data.Plot;  
+
+    poster.setAttribute("src",data.Poster)
     console.log(data.Poster);        
-    console.log(data.Title);        
-    console.log(data.imdbRating);        
-    console.log(data.Year);        
-    console.log(data.Runtime);        
-    console.log(data.Genre);        
-    console.log(data.Actors);        
-    console.log(data.Plot);        
+      
     }
 
     catch(err){
@@ -22,5 +42,4 @@ async function getData() {
     }
 }
 
-getData();
 
